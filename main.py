64537778da73bb5b670878ede8970c1c60011a2a
thetrac44-a11y <thetrac44@gmail.com>
2026-02-23 14:40:50 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,55 +1,18 @@
-import product_manager
-# from product_manager import *
-while True:
-    print("=====QUẢN LÝ CỬA HÀNG LAPTOP=====")
-    print("1. Thêm sản phẩm")
-    print("2. Hiển thị sản phẩm")
-    print("3. Tìm kiếm sản phẩm")
-    print("4. Xóa sản phẩm")
-    print("0. Thoát")
-    choice = input("Chọn chức năng: ")
+# pip install fastapi uvicorn
+# chạy code: uvicorn main:app --reload
+# Kiểm tra kết quả: trang chủ http://127.0.0.1:8000
+# Tài liệu tương tác: http://127.0.0.1:8000/docs
 
-#Thêm
-    if choice == "1":
-        product = {
-            "id": input("Nhập mã sản phẩm: "),
-            "name": input("Nhập tên sản phẩm: "),
-            "brand": input("Nhập thương hiệu: "),
-            "price": int(input("Nhập giá: ")),
-            "quantity": int(input("Nhập số lượng: "))
-        }
-        product_manager.add_product(product)
-        print("Đã thêm sản phẩm")
-# HIỂN THỊ 
-    elif choice == "2":
-        products = product_manager.show_products()
-        if len(products) == 0:
-            print(" Chưa có sản phẩm")
-        else:
-            for p in products:
-                print(p)
+from fastapi import FastAPI
 
-    #  TÌM 
-    elif choice == "3":
-        code = input("Nhập mã sản phẩm: ")
-        result = product_manager.find_product(code)
-        if result:
-            print(" Tìm thấy:", result)
-        else:
-            print(" Không tìm thấy")
+# Khởi tạo ứng dụng FastAPI
+app = FastAPI()
 
-    #  XÓA 
-    elif choice == "4":
-        code = input("Nhập mã sản phẩm: ")
-        if product_manager.delete_product(code):
-            print(" Đã xóa sản phẩm")
-        else:
-            print(" Không tìm thấy")
-#Thoát
-    elif choice == "0":
-        print("Thoát chương trình:")
-        break
-    else:
-        print("Hãy chọn lại:")
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
 
-print("Cảm ơn đã sử dụng chương trình!")
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "query": q}
+
